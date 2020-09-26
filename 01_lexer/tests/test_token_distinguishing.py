08b7974f3260fad2adc1bb890257293dbf6722ab
C. H. Lay <development@cschlay.com>
2020-09-26 18:20:13 +0300

    Add structure for distinguishing tests

---
+++ 01_lexer/tests/test_token_distinguishing.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+
+class TokenDistinguishTest(TestCase):
+    def test_function_and_variable_name(self):
+        pass
+
+    def test_keyword_and_variable_name(self):
+        pass
+
+    def test_operator_gt_and_gte(self):
+        pass
+
+    def test_operator_lt_and_lte(self):
+        pass
+
+    def test_string_literals_and_variable_name(self):
+        pass
+
+    def test_comment_and_other_code(self):
+        pass
+
+    def test_integer_literals_and_decimal_literals(self):
+        pass
+
