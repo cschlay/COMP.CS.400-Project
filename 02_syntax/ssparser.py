2447b385e3dc3ebb3af9afc7919f216c8b26e885
chi-hao.lay@outlook.com <development@cschlay.com>
2020-10-16 08:43:55 +0300

    Add basic the p_program syntax definition to ssparser.py

--- 02_syntax/ssparser.py
+++ 02_syntax/ssparser.py
@@ -1,8 +1,57 @@
-from ply import yacc
+from typing import List
+
+import ply.yacc
 
 import sslexer
 
-parser: yacc.LRParser = yacc.yacc()
+tokens: List[str] = sslexer.tokens
+
+# Alias for p arg typehint for easier development.
+P = [ply.yacc.YaccProduction]
+
+
+def p_program(p: P):
+    """program : statement_list"""
+    pass
+
+
+def p_statement_list(p: P):
+    """statement_list : statement
+                      | statement_list"""
+    pass
+
+
+def p_statement(p: P):
+    """statement : PRINT_SHEET SHEET_IDENT"""
+    statement_name = None
+    # TODO: Verify "statement name" from the staff.
+    #if p[1] == 'print_sheet':
+    #     statement_name = 'statement'
+    print(f"statement({p[1]})")
+
+
+def p_cell_ref(p: P):
+    """cell_ref : SHEET_IDENT SQUOTE COORDINATE_IDENT
+                | DOLLAR COLON RANGE_IDENT
+                | DOLLAR
+    """
+    print(p[1])
+
+
+def p_atom(p: P):
+    """atom : IDENT
+            | DECIMAL_LITERAL
+            | cell_ref
+    """
+    print("atom", p[0])
+
+
+def p_error(p: P):
+    print(f"{p.lineno}:Syntax Error (token:'{p.value}')")
+    raise SystemExit
+
+
+parser: ply.yacc.LRParser = ply.yacc.yacc()
 
 
 def parse_data(data: str):
---
+++ 02_syntax/tests/test_ssparser.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+import ssparser
+
+
+class SSParserTest(TestCase):
+    def test_statement(self):
+        # PRINT_SHEET SHEET_IDENT
+        ssparser.parse_data(data='print_sheet A')
